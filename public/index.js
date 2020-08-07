12dc46901be50ff8f53a18b8c2aa9ca942c7dc52
bhaskar sengupta <53405610+kudos-dot-com@users.noreply.github.com>
2020-08-07 13:34:13 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -3,26 +3,35 @@ async function getData()
 const data=await (await fetch("https://api.rootnet.in/covid19-in/stats/latest")).json();
 var body=document.querySelector('#tablebody');
 const dataVal=data.data.regional.map((dataset) => dataset );  
-dataset(dataVal);
 dataVal.forEach((location,index)=> {
    var newhtml=`<tr><td>${index+1}</td><td>${location.loc}</td><td>${location.confirmedCasesForeign}</td><td>${location.discharged}</td><td>${location.deaths}</td><td>${location.totalConfirmed}</td></tr>`;
    body.insertAdjacentHTML('beforeend',newhtml); 
 });
-console.log(dataVal);
+//console.log(dataVal);
+dataset(dataVal);
 }
 getData();
 
-//let dataset=[];
+const dataset_india=[];
 function dataset(dataVal)
-{
-console.log(dataVal);
+{ 
+
+document.querySelector('#input input').addEventListener("keyup",()=>{
+    var getch=document.querySelector('#input input');
+    var body=document.querySelector('#tablebody');
+    let  table=body.querySelectorAll('tr');
+ 
+        dataVal.forEach((pos,index)=>{
+        if((getch.value.trim()===pos.loc.toLowerCase().substring(0,getch.value.length).trim()))
+        {
+           table[index].style.display='';
+        }
+        else
+        { 
+            table[index].style.display='none';
+            
+        }
+    });
+});
 }
 
-
-/*document.querySelector('#input input').addEventListener("keyup",()=>{
-    console.log(document.querySelector('#input input').value);
-    const data=await (await fetch("https://api.rootnet.in/covid19-in/stats/latest")).json();
-
-    dataVal=data.data.regional.map((dataset) => dataset );  
-
-});*/
